a6b5c9f0ffcd4246f3ed63b69ebd91334606d67b
priyankpshah <priyank687@gmail.com>
2016-09-24 12:34:14 -0500

    menu & icon added

---
+++ app/src/main/java/com/intprep/priyank/musicplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.intprep.priyank.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
