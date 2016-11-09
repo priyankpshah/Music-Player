02a800822abd37663d5d8011962069e9d2aca067
Priyank Shah <priyank687@gmail.com>
2016-11-09 12:58:15 -0600

    Added new class to override MediaController Hiding after few seconds

---
+++ app/src/main/java/com/intprep/priyank/musicplayer/MusicController.java
@@ -0,0 +1,19 @@
+package com.intprep.priyank.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Priyank on 11/9/2016.
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context context) {
+        super(context);
+    }
+
+    public void hide(){
+
+    }
+}
